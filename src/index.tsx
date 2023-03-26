dbe12e10507cb3a86ae243971712e1f0c20c3496
Yannick de Lange <yannick.l.88@gmail.com>
2023-03-26 23:39:05 +0200

    Removed router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./index.less";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/routes";
+import { LandingPage } from "./pages/LandingPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <LandingPage />
   </React.StrictMode>
 );
